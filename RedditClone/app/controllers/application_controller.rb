052296d1342c61b90f010669c3b5794f4f2a3598
Travis <travishn@uci.edu>
2018-05-15 19:13:03 -0700

    Migrate user model and create application controller methods

---
+++ RedditClone/app/controllers/application_controller.rb
@@ -0,0 +1,26 @@
+class ApplicationController < ActionController::Base
+  helper_method :current_user
+
+  def current_user
+    return nil if session[:session_token].nil?
+    @current_user ||= User.find_by(session_token: session[:session_token])
+  end
+
+  def login(user)
+    session[:session_token] = user.reset_session_token!
+    @current_user = user
+  end
+
+  def logout
+    current_user.reset_session_token!
+    session[:session_token] = nil
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def require_login
+    redirect_to new_session_url unless logged_in?
+  end
+end
